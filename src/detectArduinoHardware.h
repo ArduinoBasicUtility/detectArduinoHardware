e1d4c5245cb1cdf0c4506da8f60983a29078d1f2
ぶり <houtbrion@gmail.com>
2025-10-03 14:31:38 +0900

    Update detectArduinoHardware.h
    ESP32環境でのピン番号指定方式(GPIO版)への対応を追加．

--- src/detectArduinoHardware.h
+++ src/detectArduinoHardware.h
@@ -11,7 +11,7 @@
 #define XTENSA_LX6_ARCH   5 /* ESP32 */
 #define RA4_ARCH          6 /* Renesas */
 #define STM32_ARCH        7 /* STMicro */
-#define RP_ARCH       8 /* Raspberry Pi Pico */
+#define RP_ARCH           8 /* Raspberry Pi Pico */
 
 
 #define TYPE_ATmega32U4  1
@@ -3848,6 +3848,63 @@ Labplus mPython
 #define HARDWARE_NAME "RP2040"
 #endif
 
+
+#if CPU_TYPE!=TYPE_ESP32 || ! defined(BOARD_USES_HW_GPIO_NUMBERS)
+#define D0 0
+#define D1 1
+#define D2 2
+#define D3 3
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+#define D8 8
+#define D9 9
+#define D10 10
+#define D11 11
+#define D12 12
+#define D13 13
+#define D14 14
+#define D15 15
+#define D16 16
+#define D17 17
+#define D18 18
+#define D19 19
+#define D20 20
+#define D21 21
+#define D22 22
+#define D23 23
+#define D24 24
+#define D25 25
+#define D26 26
+#define D27 27
+#define D28 28
+#define D29 29
+#define D30 30
+#define D31 31
+#define D32 32
+#define D33 33
+#define D34 34
+#define D35 35
+#define D36 36
+#define D37 37
+#define D38 38
+#define D39 39
+#define D40 40
+#define D41 41
+#define D42 42
+#define D43 43
+#define D44 44
+#define D45 45
+#define D46 46
+#define D47 47
+#define D48 48
+#define D49 49
+#define D50 50
+#define D51 51
+#define D52 52
+#define D53 53
+#endif /* CPU_TYPE!=TYPE_ESP32 || ! defined(BOARD_USES_HW_GPIO_NUMBERS) */
 /*
  * 未定義を埋める操作
  */
